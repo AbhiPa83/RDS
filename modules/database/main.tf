3d1a5c5fdbbcdcc566d6353c637299bfbe9ce444
AbhiPa83 <abhipa83@outlook.com>
2026-03-09 15:10:27 +0530

    updated region

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -24,3 +24,28 @@ resource "aws_db_instance" "mysql" {
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   skip_final_snapshot  = true
 }
+
+resource "aws_cloudwatch_metric_alarm" "rds_cpu_alarm" {
+  alarm_name = "rds-high-cpu-usage"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods = "2"
+  metric_name = "CPUUtilization"
+  namespace = "AWS/RDS"
+  period = "300"
+  statistic = "Average"
+  threshold = "60"
+  alarm_description = "The metric monitors RDS CPU utilization"
+  dimensions = {
+    DBINstanceIdentifier = aws_db_instance.mysql.id
+  }
+}
+
+resource "aws_sns_topic" "alerts" {
+  name = "rds-alerts-topic"
+}
+
+resource "aws_sns_topic_subscription" "email_sub" {
+  topic_arn = aws_sns_topic.alerts.arn
+  protocol  = "email"
+  endpoint  = "your-email@example.com" # Put your real email here
+}
